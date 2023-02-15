e97be100fea3c9317caa5c76a682da674bcca484
OleksandrVernichenko <vernichenko.aleksandr@gmail.com>
2023-02-15 14:04:46 +0100

    useCallback

--- src/hooks/likedCar.js
+++ src/hooks/likedCar.js
@@ -1,5 +1,4 @@
 import { useDispatch } from "react-redux";
-import { useCallback } from "react";
 import { useHttp } from "./http.hook";
 import { fetchUsers } from "../actions";
 
@@ -7,35 +6,28 @@ const useAddLikedCars = () => {
   const { request } = useHttp();
   const dispatch = useDispatch();
 
-  const addLikedCar = useCallback(
-    (id, user) => {
-      const currentUser = user;
-      currentUser.likedCars = [...currentUser.likedCars, id];
-      localStorage.setItem("user", JSON.stringify([currentUser]));
+  const addLikedCar = (id, user) => {
+    const currentUser = user;
+    currentUser.likedCars = [...currentUser.likedCars, id];
+    localStorage.setItem("user", JSON.stringify([currentUser]));
 
-      request(`/users/${currentUser.id}`, "PUT", JSON.stringify(currentUser))
-        .then((res) => console.log(res, "User Modified"))
-        .then(dispatch(fetchUsers(request)))
-        .catch((err) => console.log(err));
-    },
-    [request, dispatch]
-  );
+    request(`/users/${currentUser.id}`, "PUT", JSON.stringify(currentUser))
+      .then((res) => console.log(res, "User Modified"))
+      .then(dispatch(fetchUsers(request)))
+      .catch((err) => console.log(err));
+  };
 
-  const deleteLikedCar = useCallback(
-    (id, user) => {
-      const currentUser = user;
-      const filteredCars = currentUser.likedCars.filter((car) => car !== id);
-      currentUser.likedCars = filteredCars;
-      localStorage.setItem("user", JSON.stringify([currentUser]));
+  const deleteLikedCar = (id, user) => {
+    const currentUser = user;
+    const filteredCars = currentUser.likedCars.filter((car) => car !== id);
+    currentUser.likedCars = filteredCars;
+    localStorage.setItem("user", JSON.stringify([currentUser]));
 
-      request(`/users/${currentUser.id}`, "PUT", JSON.stringify(currentUser))
-        .then((res) => console.log(res, "User Modified"))
-        .then(dispatch(fetchUsers(request)))
-        .catch((err) => console.log(err));
-
-    },
-    [request, dispatch]
-  );
+    request(`/users/${currentUser.id}`, "PUT", JSON.stringify(currentUser))
+      .then((res) => console.log(res, "User Modified"))
+      .then(dispatch(fetchUsers(request)))
+      .catch((err) => console.log(err));
+  };
 
   return { addLikedCar, deleteLikedCar };
 };
